ed8fc35279361297c035ad618f3d68adc255a46a
Elena SMIRNOVA <esmirnov@e1r7p12.42.fr>
2022-01-20 16:16:39 +0100

    gnl MAJ 2022-01-20 16h16

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,36 @@
 /*   By: esmirnov <esmirnov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 12:09:39 by esmirnov          #+#    #+#             */
-/*   Updated: 2022/01/20 12:25:27 by esmirnov         ###   ########.fr       */
+/*   Updated: 2022/01/20 15:54:47 by esmirnov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	*hold_buffer;
+	char		*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	while (read(fd, buffer, BUFFER_SIZE) > 0)
+	{	
+		while (!buffer && buffer != '\n')
+		{
+			*hold_buffer = *buffer;
+		}
+		ft_strjoin(line, hold_buffer);
+	}
+	free(buffer); /* voir pour ligne et hold_buffer*/
+	return (NULL);
+}
+
+/* main zith bytes_n vriabl for return of read fonction
 
 #include "get_next_line.h"
 
@@ -30,7 +57,7 @@ char	*get_next_line(int fd)
 		bytes_n = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_n < 0)
 		{	
-			free(buffer);/* voir pour ligne et hold_buffer */
+			free(buffer);/* voir pour ligne et hold_buffer 
 			return (NULL);
 		}
 		while (!buffer && buffer != '\n')
@@ -39,4 +66,4 @@ char	*get_next_line(int fd)
 		}
 	}			
 	return ("NULL");
-}
+} */
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,28 @@
 /*   By: esmirnov <esmirnov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 12:10:00 by esmirnov          #+#    #+#             */
-/*   Updated: 2022/01/20 10:57:40 by esmirnov         ###   ########.fr       */
+/*   Updated: 2022/01/20 15:34:56 by esmirnov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 ft_free(char *buffer)
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	unsigned int	len1;
+	unsigned int	len2;
+	char			*str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	str = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (str == NULL)
+		return (NULL);
+	ft_memcpy(str, s1, len1 + 1);
+	ft_strlcat(str, s2, len1 + len2 + 1);
+	return (str);
+}
